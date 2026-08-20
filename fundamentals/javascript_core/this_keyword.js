ff6923e46c2c3185fcd4ec36a312281e2f768294
Asad-Yaqob <asadmuhammad09123@gmail.com>
2026-08-20 15:31:10 +0500

    day 4 this keyword code practice complete

--- fundamentals/javascript_core/this_keyword.js
+++ fundamentals/javascript_core/this_keyword.js
@@ -1,35 +1,93 @@
-// "use strict";
+// "use strict"
 
-// function normalFunction(){
-//     console.log(this);
+// Defination:
+// the value of this keyword is depend on how a function is being called, in most cases. it is different in global,
+// normal function, arrow function, object scope, and dom elements also in stric and non-strict mode.
+
+// Code Examples: 
+
+// Global Scope:
+// console.log("this in global scope", this); 
+
+// Normal Function Scope:
+// function testFunc(){
+//     console.log("this is normal function",this);
+// }
+// testFunc();
+
+// Call, Apply, Bind Methods:
+
+// Basic Approach
+// const person = {
+//     name: "Asad", 
+//     age: 22, 
+//     introduce: function(){
+//         console.log("Hello, Myself" + " " + this.name);
+//     }
 // }
 
-// normalFunction(); // this will refer to the global object (window in browsers) in non-strict mode, or undefined in strict mode.
+// const person2 = { name: "Ali", age: 25 };
+// const person3 = { name: "Ahmed", age: 30 };
 
-// var x = 10;
-// console.log(this);
+// person.introduce();
+// person.introduce.call(person2);
+// person.introduce.apply(person3);
 
-// const obj = {
-//     name: "Asad",
-//     greet: function() {
-//         function inner() {
-//             console.log(this.name);
-//         }
-//         inner();
+// Good Approach
+
+// function greeting(age, city){
+//     console.log("Hello, Welcome" + " " + this.name, " ", "Age:", age, "City:", city);
+// }
+
+// const asad = { name: "Asad"};
+// const ahmed = { name: "Ahmed"};
+
+// greeting.call(asad, 22, "Karachi");
+// greeting.apply(ahmed, [25, "Lahore"]);
+
+// const bindMethod = greeting.bind(asad, 22, "Karachi");
+// bindMethod();
+
+// Arrow Function Scope:
+
+// const arrFunc = () => {
+//     console.log("this in arrow function", this);
+// }
+
+// Object Scope:
+// this behavior with normal function.
+// const person = {
+//     name: "Asad", 
+//     age: 22, 
+//     introduce: function(){
+//         console.log("this is function", this);
+//        function innerFunc(){
+//             console.log("this in inner function", this);
+//        }
+//        innerFunc();
 //     }
-// };
+// }
+// person.introduce();
 
-// obj.greet(); // ?
+// this behavior with arrow function.
+const person2 = {
+    name: "Asad", 
+    age: 22, 
+    introduce: () => {
+        
+        console.log(this);
 
-
-const obj = {
-    name: "Asad",
-    greet: function() {
-        function inner() {
-            console.log(this.name);
+        function greeting(){
+            console.log("Welcome", this);
+            
         }
-        inner();
-    }
-};
+        greeting();
 
-obj.greet(); // ?
+       let innerFunc = () => {
+            console.log("this in inner function", this);
+       }
+       innerFunc();
+
+    }
+}
+person2.introduce();
